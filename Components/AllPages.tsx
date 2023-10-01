6840d5f2552b3c57d704a913f0c61f547a948602
OmarAfet <OmarAlenezy2004@gmail.com>
2023-10-01 19:31:47 +0300

    First Commit

---
+++ Components/AllPages.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function AllPages() {
+	return (
+		<>
+			<Link href="/" className="btn-click">Home</Link>
+		</>
+	);
+}
---
+++ Components/Footer.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import AllPages from "./AllPages";
+import MainLogo from "./MainLogo";
+import SwitchThemeButton from "./SwitchThemeButton";
+
+export default function Footer() {
+	return (
+		<>
+			<div className="flex flex-col mt-64">
+				<hr />
+				<div className="flex flex-col md:flex-row items-start gap-16 justify-between mx-32 my-16">
+					<MainLogo />
+					<div className="flex flex-col gap-2">
+						<div className="title">Pages</div>
+						<div className="flex flex-col gap-2 text-left items-start">
+							<AllPages />
+						</div>
+					</div>
+					<div className="flex flex-col gap-2">
+						<div className="title">Appearance</div>
+						<div className="flex flex-col gap-2 text-left items-start">
+							<div className="flex gap-2 items-center">
+								<div>Switch Theme</div>
+								<SwitchThemeButton />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
