eac5245ae0b69ff9255a1a5d2a3d032912e2c4f9
M7ilan <distny2004@gmail.com>
2023-10-02 18:20:37 +0300

    add social media icons

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import Link from "next/link";
 import AllPages from "./AllPages";
 import Logo from "./Logo";
 import SwitchThemeButton from "./SwitchThemeButton";
+import { RiTwitterXFill, RiInstagramFill, RiTiktokFill } from "react-icons/ri";
 
 export default function Footer() {
 	return (
@@ -10,7 +12,20 @@ export default function Footer() {
 			<div className="flex flex-col mt-64">
 				<hr />
 				<div className="flex flex-col md:flex-row items-start gap-16 justify-between mx-32 my-16">
-					<Logo />
+					<div className="flex flex-col gap-2">
+						<Logo />
+						<div className="flex gap-2 text-left items-start">
+							<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://x.com">
+								<RiTwitterXFill />
+							</Link>
+							<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://instagram.com">
+								<RiInstagramFill />
+							</Link>
+							<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://tiktok.com">
+								<RiTiktokFill />
+							</Link>
+						</div>
+					</div>
 					<div className="flex flex-col gap-2">
 						<div className="title">Pages</div>
 						<div className="flex flex-col gap-2 text-left items-start">
