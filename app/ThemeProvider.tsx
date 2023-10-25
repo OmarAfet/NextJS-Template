0534bd1b0333006b2cb6099bf62cb2b9f7909f1f
OmarAfet <OmarAlenezy2004@gmail.com>
2023-10-26 02:39:47 +0300

    refactor

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -3,6 +3,6 @@
 import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 
-export default Providers = ({ children }: { children: ReactNode }) => {
+export default function Providers({ children }: { children: ReactNode }) {
 	return <ThemeProvider attribute="class">{children}</ThemeProvider>;
-};
+}
