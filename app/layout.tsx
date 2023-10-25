d6c598682649a7b1aa69139c2bbeb99ecc7658d0
Omar Afet <135471287+OmarAfet@users.noreply.github.com>
2023-10-26 01:23:51 +0300

    testing remote repo in VSCode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import Navbar from "@Components/Navbar";
-import Footer from "@Components/Footer";
-import Providers from "./Provider";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
+import ThemeProviders from "./Provider";
 import type { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
 
@@ -15,11 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en" className="!scroll-smooth">
 			<body className="bg-white text-OpenColor-gray-7 dark:bg-OpenColor-gray-9 dark:text-OpenColor-gray-1">
 				<Toaster position="top-center" reverseOrder={false} />
-				<Providers>
+				<ThemeProviders>
 					<Navbar />
 					<main className="mt-8 px-16 container m-auto">{children}</main>
 					<Footer />
-				</Providers>
+				</ThemeProviders>
 			</body>
 		</html>
 	);
