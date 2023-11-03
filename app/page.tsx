39e60fb4bdfcd92a3d890ef097de2af3fb47f823
Omar Afet <135471287+OmarAfet@users.noreply.github.com>
2023-11-04 02:29:47 +0300

    link my account

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
 	return (
 		<div className="flex flex-col gap-2">
 			<div className="header center">Home Page</div>
-			<div className="subtitle center">made by OmarAfet</div>
+			<div className="subtitle center">made by <Link className="ml-1 btn-click" href="https://github.com/OmarAfet">OmarAfet</Link></div>
 		</div>
 	);
 }
