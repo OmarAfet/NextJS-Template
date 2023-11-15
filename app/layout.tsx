822678778d81a33f18ce46a1d71e3e2d15336908
OmarAfet <OmarAlenezy2004@gmail.com>
2023-11-15 03:06:19 +0300

    Update font and body styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import Footer from "@components/Footer";
 import ThemeProviders from "./ThemeProvider";
 import type { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
+import { Ubuntu } from "next/font/google";
+
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["300", "400", "500", "700"] });
 
 export const metadata: Metadata = {
 	title: "NextJS Template by OmarAfet",
@@ -13,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="!scroll-smooth" suppressHydrationWarning>
-			<body className="bg-white dark:bg-OpenColor-gray-9 text-OpenColor-gray-7 dark:text-OpenColor-gray-1">
+			<body className={`${ubuntu.className} bg-white dark:bg-OpenColor-gray-9 text-OpenColor-gray-7 dark:text-OpenColor-gray-1`}>
 				<Toaster position="top-center" reverseOrder={false} />
 				<ThemeProviders>
 					<Navbar />
