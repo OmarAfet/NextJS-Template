c00b510ea260dfc71d2d000b8be8ca1ff06764e2
M7ilan <distny2004@gmail.com>
2023-11-18 16:41:15 +0300

    Add ToastPromiseType definition

---
+++ types/ToastPromiseType.ts
@@ -0,0 +1,8 @@
+type ToastPromiseType = (
+	customFunction: () => Promise<any>,
+	loadingMessage: string,
+	successMessage: string,
+	errorMessage: string,
+) => Promise<any>;
+
+export default ToastPromiseType;
