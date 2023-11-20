feefc8afbd1515303c2a0070b3e5011f951c05fa
M7ilan <distny2004@gmail.com>
2023-11-20 16:05:13 +0300

    Update Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo({ classname }: { classname?: string }) {
 	return (
 		<Link href="/" className={`text-4xl font-bold cursor-pointer ${classname}`}>
-			Bungie API
+			Logo
 		</Link>
 	);
 }
