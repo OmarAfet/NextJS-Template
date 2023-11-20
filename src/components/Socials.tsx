79341174fb289803c3cfa2746dec0a9fe1a0edb0
M7ilan <distny2004@gmail.com>
2023-11-21 01:43:44 +0300

    Fix Socials component size prop

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { RiGithubFill, RiTiktokFill, RiTwitterXFill } from "react-icons/ri";
 
-export default function Socials({ size }: { size: number }) {
-	const customSize = `${size}px`;
+export default function Socials({ size }: { size?: number }) {
+	const customSize = `${size || "32"}px`;
 
 	return (
 		<div className="flex gap-2 text-left items-center">
