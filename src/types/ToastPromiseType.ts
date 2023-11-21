ec6291ea57fad0b4cac97b04c9b3c21271117917
M7ilan <distny2004@gmail.com>
2023-11-21 06:09:18 +0300

    Remove export statement from ToastPromiseType

--- src/types/ToastPromiseType.ts
+++ src/types/ToastPromiseType.ts
@@ -1,3 +1,1 @@
 type ToastPromiseType = (customFunction: () => Promise<any>, loadingMessage: string, successMessage: string, errorMessage: string) => Promise<any>;
-
-export default ToastPromiseType;
