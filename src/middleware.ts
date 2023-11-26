26742bf0eef5cdd27a4ea5f7e641f8669fdb3dbe
M7ilan <distny2004@gmail.com>
2023-11-26 07:32:54 +0300

    Remove middleware file

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-// Middleware Docs - https://nextjs.org/docs/app/building-your-application/routing/middleware
-
-import { NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-	// console.log("middleware");
-}
