ad885cf3cf5435f8a90ea05dfc6368ad7e7d2d3b
M7ilan <distny2004@gmail.com>
2023-12-19 23:12:37 +0300

    Refactor layout component to use MainProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "@/src/styles/globals.scss";
 import Navbar from "@/src/components/Header";
 import Footer from "@/src/components/Footer";
-import ThemeProviders from "@/src/providers/ThemeProvider";
 import type { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
 import { Inter } from "next/font/google";
+import MainProvider from "../providers/MainProvider";
 
 const font = Inter({
 	subsets: ["latin"],
@@ -24,11 +24,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en" className="!scroll-smooth" suppressHydrationWarning>
 			<body className={`${font.className} flex flex-col min-h-screen`}>
 				<Toaster position="top-center" reverseOrder={false} />
-				<ThemeProviders>
+				<MainProvider>
 					<Navbar />
 					<main className="flex-1">{children}</main>
 					<Footer />
-				</ThemeProviders>
+				</MainProvider>
 			</body>
 		</html>
 	);
